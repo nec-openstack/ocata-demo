3779d74c2125527b9607c793e31b76aa4da38d9f
OTSUKA, Yuanying <yuanying@fraction.jp>
2016-09-27 10:19:42 +0900

    Make manager node as drain mode

--- heat/fragments/00-configure-docker.sh
+++ heat/fragments/00-configure-docker.sh
@@ -8,3 +8,8 @@ echo "export https_proxy=${HTTPS_PROXY}" >> /etc/default/docker
 echo "DOCKER_OPTS='-H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375'" >> /etc/default/docker
 
 service docker restart
+
+sleep 3
+
+docker swarm init
+docker node update --availability drain `hostname`
--- heat/fragments/wc-notify-master.sh
+++ heat/fragments/wc-notify-master.sh
@@ -2,13 +2,6 @@
 
 . /etc/sysconfig/heat-params
 
-# until curl -sf "http://127.0.0.1:8080/healthz"; do
-#     echo "Waiting for Kubernetes API..."
-#     sleep 5
-# done
-
-docker swarm init
-
 SWARM_WORKER_TOKEN=`docker swarm join-token -q worker`
 SWARM_MANAGER_TOKEN=`docker swarm join-token -q manager`
 
