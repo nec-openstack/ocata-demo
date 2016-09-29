f27eb4186ba7623e59e37bfe9c5a7b7965cf8cdb
OTSUKA, Yuanying <yuanying@fraction.jp>
2016-09-29 17:00:52 +0900

    Add proxy to docker daemon

--- heat/fragments/00-configure-docker.sh
+++ heat/fragments/00-configure-docker.sh
@@ -4,6 +4,9 @@
 
 echo "export http_proxy=${HTTP_PROXY}" >> /etc/default/docker
 echo "export https_proxy=${HTTPS_PROXY}" >> /etc/default/docker
+echo "http_proxy=$http_proxy" >> /etc/default/docker
+echo "https_proxy=$https_proxy" >> /etc/default/docker
+
 # WARNING: Listen all interface unsafely
 echo "DOCKER_OPTS='-H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375'" >> /etc/default/docker
 
