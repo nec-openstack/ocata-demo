f19279cbc5a455da0080933cc27f6e26dfeae194
OTSUKA, Yuanying <yuanying@fraction.jp>
2016-09-30 15:01:10 +0900

    Add deregister

--- heat/fragments/00-install-consul-server.sh
+++ heat/fragments/00-install-consul-server.sh
@@ -9,3 +9,8 @@ docker run -d --net=host --name consul-server \
     -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true}' \
     consul agent -server -bind=0.0.0.0 -advertise=${MANAGER_IP} \
                 -bootstrap-expect=1 -ui
+
+docker run -d --net=host \
+    --name deregister \
+    -p 4567:4567 \
+    yuanying/deregister
