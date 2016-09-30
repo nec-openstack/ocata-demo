09e87fb04bdf9c1a7a6ebbc53eb04d3c442611cd
OTSUKA, Yuanying <yuanying@fraction.jp>
2016-09-30 14:54:28 +0900

    Update deregister

--- deregister/server.rb
+++ deregister/server.rb
@@ -2,8 +2,21 @@
 
 require 'sinatra'
 require 'json'
+require 'rest-client'
 
 post '/' do
   params = JSON.parse request.body.read
-  p params
+  alerts = params['alerts']
+  alerts.each do |alert|
+    if alert["labels"]["job"] == "node"
+      payload = {
+        "Node" => alert["labels"]["instance"]
+      }
+      RestClient.put(
+        "#{ENV['CONSUL_URL']}/v1/catalog/deregister",
+        payload.to_json,
+        {content_type: :json, accept: :json}
+      )
+    end
+  end
 end
