4945aec6df0394fdf3ddc4db8e25ad198184206c
OTSUKA, Yuanying <yuanying@fraction.jp>
2016-10-04 17:53:51 +0900

    Support docker registry to cache image

--- heat/fragments/00-configure-docker.sh
+++ heat/fragments/00-configure-docker.sh
@@ -16,3 +16,16 @@ docker swarm init
 
 sleep 3
 docker node update --availability drain `hostname`
+
+REGISTRY_CONF_DIR="/srv/docker/registry"
+REGISTRY_CONF=${REGISTRY_CONF_DIR}/config.yml
+
+mkdir -p ${REGISTRY_CONF_DIR}
+cat > ${REGISTRY_CONF} <<'EOF'
+$REGISTRY_CONFIG
+EOF
+
+docker run -d --restart=always \
+    -p 5000:5000 --name v2-mirror \
+    -v ${REGISTRY_CONF_DIR}:/var/lib/registry \
+    registry:2.3 /var/lib/registry/config.yml
